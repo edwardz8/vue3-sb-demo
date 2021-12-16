f218631332e8bfa9fde89b570426e144010da733
edwardz8 <edwardz8@yahoo.com>
2021-12-16 00:28:22 -0500

    add grid

--- src/components/ProductGrid.vue
+++ src/components/ProductGrid.vue
@@ -4,44 +4,40 @@ export default {
     blok: {
       type: Object,
       required: true
-    },
-    products: { type: Object },
-    categories: { type: Object },
-  },
-  data() {
-    return {
-      fullCategories: [],
-      fullProducts: []
     }
-  }
+  },
 }
 </script>
 
 <template>
-  <ul v-editable="blok" class="section">
-    <li
+  <div v-editable="blok" class="section"> 
+    <button
       :key="category.id"
       v-for="category in blok.categories.items"
-      class="container"
-    >{{ category.name }}</li>
-  </ul>
+      type="button"
+      class="btn bg-light mx-2 my-2"
+    >
+      {{ category.name }}
+    </button>
+  </div>
+
   <div v-editable="blok" class="grid">
-     <div :key="product.name" v-for="product in blok.products.items" class="container"> 
-     <div class="card mx-2 my-2" style="width: 18rem">
+    <div :key="product.name" v-for="product in blok.products.items" class="container">
+      <div class="card mx-2 my-2" style="width: 18rem">
         <img :src="product.image" class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">{{ product.name }}</h5>
-          <!-- <a class="btn btn-primary">Info</a> -->
+          <a class="btn btn-secondary">View</a>
         </div>
       </div>
-     </div>
+    </div>
   </div>
   <!-- <div v-editable="blok" class="section">
     <pre><code>{{blok}}</code></pre>
   </div>-->
 </template>
 
-<style> 
+<style>
 .grid {
   display: flex;
   flex-wrap: wrap;
